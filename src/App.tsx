31c5f00356c2a872f2f13a4dab57bab636bbf4e7
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-26 13:23:21 -0300

    chore: Add Routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
-    <h1>Hello World</h1>
+  <Router>
+    <Routes />
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
