27cb7b6bd04b05e682a7b5d1c02818dc2f11386a
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-26 06:48:02 -0300

    chore: Create global style

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    margin: 0;
+  }
+
+  body {
+    background: #F0F2F5;
+    color: #1C1C1C;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 700
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
