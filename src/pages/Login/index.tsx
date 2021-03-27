7cd3112d306dfac52c827d83fb6ae22c062062e6
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-27 11:33:40 -0300

    chore: Add login to routes

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Login: React.FC = () => <h1>ok</h1>;
+
+export default Login;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Login from '../pages/Login';
+
 const routes: React.FC = () => (
   <Switch>
     <Route path="/" exact>
       <h1>ok</h1>
     </Route>
+    <Route path="/login" component={Login} />
   </Switch>
 );
 
