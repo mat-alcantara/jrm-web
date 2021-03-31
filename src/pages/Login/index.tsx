3ef64cc9f236792f27bea20864c7492b1920b8e0
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-31 15:07:49 -0300

    chore: Create layout of login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import Logo from '../../assets/logo.svg';
+
 const Login: React.FC = () => (
   <Container>
     <form>
-      <h1>Login</h1>
-
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <img src={Logo} alt="JRM Compensados" />
+      <h1>Faça o seu Login</h1>
+      <input type="text" name="email" placeholder="Digite o seu email" />
+      <input type="password" name="password" placeholder="Digite a sua senha" />
       <button type="submit">Entrar</button>
     </form>
   </Container>
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  margin-top: 64px;
+
   width: 100%;
   display: flex;
   justify-content: center;
@@ -14,18 +16,21 @@ export const Container = styled.div`
 
     h1 {
       margin: 64px auto 16px auto;
+      font-size: 48px;
     }
 
     input {
       text-align: center;
-      min-width: 350px;
+      width: 535px;
       margin-bottom: 8px;
-      height: 32px;
+      height: 53px;
+      font-size: 24px;
     }
 
     button {
-      min-width: 350px;
-      padding: 8px 0;
+      width: 300px;
+      height: 53px;
+      background-color: #e76905;
     }
   }
 `;
