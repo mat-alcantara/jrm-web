43f2652cf59231e7cfaa6ba9cd6139da8b9921dc
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-01 19:17:40 -0300

    chore: Remove Formik tutorial from routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
-import FormikTutorial from '../pages/FormikTutorial';
 
 const routes: React.FC = () => (
   <Switch>
@@ -10,7 +9,6 @@ const routes: React.FC = () => (
       <h1>ok</h1>
     </Route> */}
     <Route path="/" exact component={Login} />
-    <Route path="/formik" component={FormikTutorial} />
   </Switch>
 );
 
