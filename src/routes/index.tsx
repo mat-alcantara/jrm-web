6c7433d61563c0b1418bb246fa7b3426340d0862
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-22 05:35:54 -0300

    chore: Add customerslist to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,15 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 
 import Dashboard from '../pages/Dashboard';
-import CreateCustomer from '../pages/Dashboard/Customers/CreateCustomer';
+import NewCustomer from '../pages/NewCustomer';
+import CustomersList from '../pages/CustomersList';
 
 const routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
-    <Route path="/newcustomer" exact component={CreateCustomer} isPrivate />
+    <Route path="/newcustomer" exact component={NewCustomer} isPrivate />
+    <Route path="/customerslist" exact component={CustomersList} isPrivate />
   </Switch>
 );
 
