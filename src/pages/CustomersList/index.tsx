26d19ce9e0c92775f74f6c0f3812ff9ef3a401f0
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-22 06:12:22 -0300

    chore: Load all customers after render

--- src/pages/CustomersList/index.tsx
+++ src/pages/CustomersList/index.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+// import {List} from 'antd';
+
+import api from '../../services/api';
 
 import AntDashboard from '../../components/AntDashboard';
 import AntContent from '../../components/AntContent';
 
+import { Container } from './styles';
+
+interface ICustomersProps {
+  id: string;
+  name: string;
+  telephone: string;
+  email: string;
+  street: string;
+  area: string;
+  city: string;
+  state: string;
+  created_at: string;
+  updated_at: string;
+}
+
 const CustomersList: React.FC = () => {
+  const [allCustomers, setAllCustomers] = useState<ICustomersProps[]>([]);
+  const token = localStorage.getItem('@JRMCompensados:token');
+
+  const getAllCustomersFromApi = useCallback(async () => {
+    const allCustomersFromApi = await api.get<ICustomersProps[]>('/customers', {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    });
+
+    setAllCustomers([...allCustomersFromApi.data]);
+  }, [allCustomers]);
+
+  useEffect(() => {
+    getAllCustomersFromApi();
+  }, []);
+
   return (
     <AntDashboard>
       <AntContent>
-        <h1>Lista de clientes</h1>
+        <Container>Lista de clientes</Container>
       </AntContent>
     </AntDashboard>
   );
