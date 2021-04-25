5733dc294f00ba0879274ade189c99e93ff4664a
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-25 10:26:43 -0300

    chore: Change Dashboard layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Layout } from 'antd';
 
 import AntDashboard from '../../components/AntDashboard';
+import AntContent from '../../components/AntContent';
 
 const Dashboard: React.FC = () => {
-  const { Content } = Layout;
-
   return (
     <AntDashboard>
-      <Content style={{ marginTop: '16px', marginBottom: '16px' }}>
-        <div className="site-layout-content">
-          <h1>Dashboard</h1>
-        </div>
-      </Content>
+      <AntContent>
+        <div />
+      </AntContent>
     </AntDashboard>
   );
 };
