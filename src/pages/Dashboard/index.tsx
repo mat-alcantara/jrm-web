f03cc2157ca20c00cc2c1d9e83679ca4eafd7173
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-05-11 07:49:33 -0300

    chore: Add basic statistics

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Statistic, Row, Col, Typography } from 'antd';
+
+import { useOrder } from '../../hooks/Order';
 
 import { Container } from './styles';
 
 import AntDashboard from '../../components/AntDashboard';
 import AntContent from '../../components/AntContent';
 
+import IOrder from '../../types/IOrder';
+
 const Dashboard: React.FC = () => {
+  const { loadOrders } = useOrder();
+
+  const [allOrders, setAllOrders] = useState<IOrder[]>([]);
+  const [deliveryAllowed, setDeliveryAllowed] = useState(0);
+  const [totalValue, setTotalValue] = useState(0);
+
+  useEffect(() => {
+    async function loadOrdersFromHook() {
+      const allOrdersFromHook = await loadOrders();
+
+      setAllOrders([...allOrdersFromHook]);
+
+      const pedidosLiberadosParaTransporte = allOrdersFromHook.filter(
+        (order) => order.orderStatus === 'Liberado para Transporte',
+      );
+
+      setDeliveryAllowed(pedidosLiberadosParaTransporte.length);
+
+      allOrdersFromHook.forEach((order) =>
+        setTotalValue((prevVal) => prevVal + order.price),
+      );
+    }
+
+    loadOrdersFromHook();
+  }, []);
+
   return (
     <AntDashboard>
       <AntContent>
         <Container>
-          <h1>Dashboard</h1>
-          <p>
-            Página dedicada a exibir informações. Ex: Cortes em produção,
-            liberados para entrega, quantidade de cortes, materiais necessários,
-            etc...
-          </p>
+          <Row align="middle" justify="center">
+            <Col span={24}>
+              <Typography.Title level={2}>Estatísticas</Typography.Title>
+            </Col>
+          </Row>
+          <Row
+            gutter={16}
+            style={{ marginTop: '32px' }}
+            align="middle"
+            justify="center"
+          >
+            <Col span={6}>
+              <Statistic
+                title="Cortes Liberados para transporte"
+                value={deliveryAllowed}
+              />
+            </Col>
+            <Col span={6}>
+              <Statistic title="Cortes totais" value={allOrders.length} />
+            </Col>
+
+            <Col span={6}>
+              <Statistic title="Valor total" value={totalValue} />
+            </Col>
+          </Row>
         </Container>
       </AntContent>
     </AntDashboard>
