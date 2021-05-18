1d0df20ffcabf6767438a8c25a4a9516f4530393
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-05-18 06:18:49 -0300

    chore: Add Ant Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.less'; // CSS files of antd
+import 'antd-mobile/dist/antd-mobile.css'; // CSS files of antd mobile
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
