a848107c6b8813460efda71cb4660e50b35af490
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-07-03 07:41:05 -0300

    chore: Finish structure of pdf

--- src/components/OrderResume/index.tsx
+++ src/components/OrderResume/index.tsx
@@ -1,9 +1,8 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import { Button, Divider, Typography, List } from 'antd';
 import { FaWhatsapp } from 'react-icons/fa';
 import React, { useRef } from 'react';
 import { useReactToPrint } from 'react-to-print';
-
-import Logo from '../../assets/logo.svg';
 
 import { Container } from './styles';
 
@@ -14,139 +13,298 @@ const Tags: React.FC = () => {
     content: () => componentRef.current,
   });
 
+  const listData = [
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+    {
+      key: '1',
+      title:
+        '1 - MDF BRANCO TX 2 FACES COMUM 15MM - 600 [2] x 500 [2] - R$ 30,00',
+    },
+  ];
+
   return (
     <>
       <div>
-        <Container ref={componentRef}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <img
-              src={Logo}
-              alt="Logotipo"
-              style={{ width: '150px', height: 'auto', marginBottom: '16px' }}
-            />
-            <Typography.Title
-              type="secondary"
-              level={2}
-              style={{ marginTop: '0' }}
-            >
-              PEDIDO DE CORTE #17
-            </Typography.Title>
-          </div>
-          <Divider style={{ margin: '0' }} />
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              fontSize: '10px',
-            }}
-          >
+        <Container
+          ref={componentRef}
+          style={{ height: listData.length > 16 ? '200vh' : '100vh' }}
+        >
+          <div>
             <div
               style={{
                 display: 'flex',
                 flexDirection: 'row',
-                justifyContent: 'center',
-                marginTop: '16px',
-                gap: '32px',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              {/* <img
+              src={Logo}
+              alt="Logotipo"
+              style={{ width: '150px', height: 'auto', marginBottom: '16px' }}
+            /> */}
+              <Typography.Title level={3} style={{ marginTop: '0' }}>
+                JRM Compensados
+              </Typography.Title>
+              <Typography.Title
+                type="secondary"
+                level={3}
+                style={{ marginTop: '0' }}
+              >
+                PEDIDO DE CORTE
+              </Typography.Title>
+            </div>
+            <Divider style={{ margin: '0' }} />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                fontSize: '10px',
               }}
             >
               <div
                 style={{
                   display: 'flex',
                   flexDirection: 'column',
-                  textAlign: 'center',
-                  marginTop: '0px',
+                  marginTop: '8px',
+                  gap: '8px',
                 }}
               >
-                <Typography.Text>
-                  Rua Julieta Conceição Reis 280
-                </Typography.Text>
-                <Typography.Text>Frade, Angra dos Reis - RJ</Typography.Text>
-                <Typography.Text
+                <div
                   style={{
                     display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    margin: '0 auto',
-                    gap: '8px',
+                    flexDirection: 'column',
+                    marginTop: '0px',
                   }}
                 >
-                  <FaWhatsapp />
-                  (24) 99964-4953
-                </Typography.Text>
+                  <Typography.Text>
+                    Rua Julieta Conceição Reis 280
+                  </Typography.Text>
+                  <Typography.Text>Frade, Angra dos Reis - RJ</Typography.Text>
+                  <Typography.Text
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      gap: '8px',
+                    }}
+                  >
+                    <FaWhatsapp />
+                    (24) 99964-4953
+                  </Typography.Text>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <Typography.Text>Endereço da Japuíba, 999</Typography.Text>
+                  <Typography.Text>
+                    Japuíba, Angra dos Reis - RJ
+                  </Typography.Text>
+                  <Typography.Text
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      gap: '8px',
+                    }}
+                  >
+                    <FaWhatsapp />
+                    (24) 99969-4543
+                  </Typography.Text>
+                </div>
               </div>
               <div
                 style={{
                   display: 'flex',
                   flexDirection: 'column',
+                  marginTop: '8px',
+                  gap: '8px',
                   textAlign: 'center',
+                  fontSize: '16px',
                 }}
               >
-                <Typography.Text>Endereço da Japuíba, 999</Typography.Text>
-                <Typography.Text>Japuíba, Angra dos Reis - RJ</Typography.Text>
-                <Typography.Text
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    gap: '8px',
-                    margin: '0 auto',
-                  }}
-                >
-                  <FaWhatsapp />
-                  (24) 99969-4543
-                </Typography.Text>
+                <div>
+                  <Typography.Title level={5} style={{ marginBottom: '0px' }}>
+                    Número do pedido
+                  </Typography.Title>
+                  <Divider style={{ margin: '0px 0px 4px 0px' }} />
+                  <Typography.Text>17</Typography.Text>
+                </div>
+                <div>
+                  <Typography.Title level={5} style={{ marginBottom: '0px' }}>
+                    Data do Pedido
+                  </Typography.Title>
+                  <Divider style={{ margin: '0px 0px 4px 0px' }} />
+                  <Typography.Text>01/07/2021</Typography.Text>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginTop: '48px',
-            }}
-          >
             <div
-              style={{ width: '48%', display: 'flex', flexDirection: 'column' }}
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: '16px',
+              }}
             >
-              <Typography.Title level={5}>Dados</Typography.Title>
-              <Divider style={{ margin: '0px 0px 8px 0px' }} />
+              <div
+                style={{
+                  width: '48%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Typography.Title level={5} style={{ marginBottom: '0px' }}>
+                  Pedido
+                </Typography.Title>
+                <Divider style={{ margin: '0px 0px 8px 0px' }} />
 
-              <Typography.Text>
-                Tipo de Entrega: Retirar na Loja
-              </Typography.Text>
+                <Typography.Text>
+                  Tipo de Entrega: Retirar na Loja
+                </Typography.Text>
 
-              <Typography.Text>Loja do Pedido: Japuíba</Typography.Text>
-              <Typography.Text>
-                Status do Pagamento: Receber na Entrega
-              </Typography.Text>
-              <Typography.Text>Prazo: Até 08/07/21</Typography.Text>
-              <Typography.Text>Vendedor: Vitória</Typography.Text>
+                <Typography.Text>Loja do Pedido: Japuíba</Typography.Text>
+                <Typography.Text>
+                  Status do Pagamento: Receber na Entrega
+                </Typography.Text>
+                <Typography.Text>Vendedor: Vitória</Typography.Text>
+                <Typography.Text>
+                  Observações: Todas estas questões, devidamente ponderadas,
+                  levantam dúvidas sobre se a estrutura atual da organização
+                  desafia a capacidade de equalização das condições
+                  inegavelmente apropriadas.
+                </Typography.Text>
+                <Typography.Text style={{ fontWeight: 'bold' }}>
+                  Prazo: Até 08/07/21
+                </Typography.Text>
+              </div>
+              <div
+                style={{
+                  width: '48%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Typography.Title style={{ marginBottom: '0px' }} level={5}>
+                  Cliente
+                </Typography.Title>
+                <Divider style={{ margin: '0px 0px 8px 0px' }} />
+                <Typography.Text>Nome: Diego Bu</Typography.Text>
+                <Typography.Text>
+                  Endereço: Endereço não informado, Japuíba
+                </Typography.Text>
+
+                <Typography.Text>Telefone: (21) 98660 - 1910</Typography.Text>
+              </div>
             </div>
-            <div
-              style={{ width: '48%', display: 'flex', flexDirection: 'column' }}
-            >
-              <Typography.Title style={{ marginBottom: '8px' }} level={5}>
-                Cliente
+            <List
+              size="small"
+              dataSource={listData}
+              itemLayout="horizontal"
+              footer={<div style={{ textAlign: 'right' }}>Total: R$ 50,00</div>}
+              style={{ width: '100%', margin: '24px auto 0px auto' }}
+              renderItem={(item) => (
+                <List.Item style={{ textAlign: 'left' }}>
+                  <List.Item.Meta
+                    title={
+                      <Typography style={{ fontSize: '10px' }}>
+                        {item.title}
+                      </Typography>
+                    }
+                  />
+                </List.Item>
+              )}
+            />
+          </div>
+          <div>
+            <div style={{ width: '60%' }}>
+              <Typography.Title level={5} style={{ marginBottom: '32px' }}>
+                Assinatura do cliente
               </Typography.Title>
-              <Divider style={{ margin: '0px 0px 8px 0px' }} />
-              <Typography.Text>Nome: Diego Bu</Typography.Text>
-              <Typography.Text>
-                Endereço: Endereço não informado, Japuíba
-              </Typography.Text>
-
-              <Typography.Text>Telefone: (21) 98660 - 1910</Typography.Text>
+              <Divider
+                style={{ margin: '0', width: '100%', background: 'black' }}
+              />
             </div>
           </div>
-          <List />
         </Container>
       </div>
 
--- src/components/OrderResume/styles.ts
+++ src/components/OrderResume/styles.ts
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 32px 64px;
-  font-size: 12px;
+  font-size: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   @media print {
     html,
